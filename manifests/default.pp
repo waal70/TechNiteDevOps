6da3f84ae89ca661d54039e34a69c6307b813e38
André <waal70@hotmail.com>
2016-04-18 14:56:43 +0200

    Version without block comment

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,14 +1,12 @@
-=begin
 group { 'awaal':
   			  ensure => 'present',
   			  gid    => '1001',
      }
-user { "awaal": 
-	ensure => present,
-	home => "/home/awaal",
-	groups => "awaal",
-	managehome => true,
-	password => '$1$8FbWib7X$Fz9HM7sjC326pC3fmUxZu0',
-	shell => "/bin/bash",
-	}
-=end
+#user { "awaal": 
+#	ensure => present,
+#	home => "/home/awaal",
+#	groups => "awaal",
+#	managehome => true,
+#	password => '$1$8FbWib7X$Fz9HM7sjC326pC3fmUxZu0',
+#	shell => "/bin/bash",
+#	}
