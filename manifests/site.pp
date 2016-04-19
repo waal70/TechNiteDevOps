5de7c9f04abde4b00e8130dc0760d8812036cf98
André <waal70@hotmail.com>
2016-04-19 14:35:26 +0200

    Scriptprep

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,19 +1,15 @@
-# TechNiteDevOps general class
-# The resources defined here are applied to ALL servers
-
 notify {"I'm notifying you.":} # Whitespace is fungible, remember.
-notify {"So am I!":}
 
 group { 'awaal':
   			  ensure => 'present',
   			  gid    => '1001',
      }
 
-  user { 'awaal':
-    ensure             => present,
-    comment  	       => 'Andre de Waal',
-    password           => '$1$8FbWib7X$Fz9HM7sjC326pC3fmUxZu0',
-    managehome         => true,
-    groups             => 'awaal',
-	shell			   => "/bin/bash",
-  }
+#  user { 'awaal':
+#    ensure             => present,
+#    comment  	       => 'Andre de Waal',
+#    password           => '$1$8FbWib7X$Fz9HM7sjC326pC3fmUxZu0',
+#    managehome         => true,
+#    groups             => 'awaal',
+#	shell			   => "/bin/bash",
+#  }
